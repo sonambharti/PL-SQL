5002101ab62fadae911ec4aaa731f5e63355270f
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2023-07-26 18:51:18 +0530

    Declare variables

--- Basics/declareVariables.sql
+++ Basics/declareVariables.sql
@@ -2,12 +2,12 @@
 
 SET SERVEROUTPUT ON
 DECLARE
-var1 VARCHAR2(10);
-num1 number(3);
+    var1 VARCHAR2(10);
+    num1 number(3);
     
 BEGIN
-var1 := 'Sonam';
-num1 := 100;
-DBMS_OUTPUT.PUT_LINE('var1: ' || var1);
-DBMS_OUTPUT.PUT_LINE('num1: ' || num1);
+    var1 := 'Sonam';
+    num1 := 100;
+    DBMS_OUTPUT.PUT_LINE('var1: ' || var1);
+    DBMS_OUTPUT.PUT_LINE('num1: ' || num1);
 END;
