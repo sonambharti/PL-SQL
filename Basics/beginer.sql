b7d4d122bae751ea843583e1799670f03ef7b208
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2023-07-26 18:50:30 +0530

    Basic beginer PL-SQL code

--- Basics/beginer.sql
+++ Basics/beginer.sql
@@ -1,3 +1,4 @@
+-- It will not print the output statement. We need to set the serveroutput on for the same.
 BEGIN
-DBMS_OUTPUT.PUT_LINE('Welcome to PL/SQL Learning');
+  DBMS_OUTPUT.PUT_LINE('Welcome to PL/SQL Learning');
 END;
